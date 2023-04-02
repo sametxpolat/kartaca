b0b47cd5c79fb63ba99d44d24516e72696803ae4
COWVNDVNTE <110100265+sametxpolat@users.noreply.github.com>
2023-04-03 00:24:47 +0300

    Delete backend directory

--- backend/src/main/java/com/kartaca/softwaredeveloper/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kartaca.softwaredeveloper.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kartaca.softwaredeveloper.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-      
-}
